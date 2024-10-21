fed63866612b5b94f83ffcafcacfa8d2a2a4af9c
viralsanghavi <vsanghavi3@gmail.com>
2024-10-22 00:26:20 +0530

    Updated teams design

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
+import Home from ".";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({Component, pageProps}) {
+  return (
+    <Home>
+      <Component {...pageProps} />
+    </Home>
+  );
 }
